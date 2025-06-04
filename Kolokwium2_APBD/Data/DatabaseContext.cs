b75f3b37f5e111a5952d2335a73222cc2dd4e188
Piotr Ogrodnik <s31824@pjwstk.edu.pl>
2025-06-04 19:07:18 +0200

    final final

--- Kolokwium2_APBD/Data/DatabaseContext.cs
+++ Kolokwium2_APBD/Data/DatabaseContext.cs
@@ -8,9 +8,45 @@ public class DatabaseContext : DbContext
     public DbSet<Customer> Customers { get; set; }
     public DbSet<Concert> Concerts { get; set; }
     public DbSet<Ticket> Tickets { get; set; }
-    public object PurchasedTickets { get; set; }
-
-    protected DatabaseContext() { }
+    public DbSet<PurchasedTicket> PurchasedTickets { get; set; }
     
-    public DatabaseContext(DbContextOptions options) : base(options) { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Concert>().HasData(
+            new Concert
+            {
+                ConcertId = 1,
+                Name = "Summer Rock Festival",
+                Date = new DateTime(2025, 7, 15),
+                AvailableTickets = 150
+            }
+        );
+        modelBuilder.Entity<Customer>().HasData(
+            new Customer
+            {
+                CustomerId = 1,
+                FirstName = "Jan",
+                LastName = "Kowalski",
+                PhoneNumber = "555-123-456"
+            }
+        );
+        modelBuilder.Entity<Ticket>().HasData(
+            new Ticket
+            {
+                TicketId = 1,
+                SerialNumber = "SN123456",
+                SeatNumber = 12,
+            }
+        );
+        modelBuilder.Entity<Ticket_Concert>().HasData(
+            new Ticket_Concert
+            {
+                TicketConcertId = 1,
+                TicketId = 1,
+                ConcertId = 1,
+                Price = 99
+            }
+        );
+    }
+    
 }
